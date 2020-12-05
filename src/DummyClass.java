5aa92ef53e995cf40e580974deeca23d8bb2ae48
LorenzoT8942 <56634954+LorenzoT8942@users.noreply.github.com>
2020-12-05 19:46:24 +0100

    first commit

---
+++ src/DummyClass.java
@@ -0,0 +1,12 @@
+
+public class DummyClass {
+	
+	private String dummyString;
+	private int dummyInt;
+	
+	public DummyClass() {
+		dummyString = "This is a dummy STRING";
+		dummyInt = 83422;
+	}
+
+}
